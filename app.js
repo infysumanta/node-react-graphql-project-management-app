7980880ba8da6ae12ce93711c04de9583b45d237
Sumanta Kabiraj <sumantablog@gmail.com>
2023-01-07 13:42:48 +0530

    schema and graphql setup

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
 const express = require("express");
+const { graphqlHTTP } = require("express-graphql");
 
+const schema = require("./schema");
 const app = express();
+
+app.use(
+  "/graphql",
+  graphqlHTTP({
+    schema: schema,
+    graphiql: true,
+  })
+);
 
 module.exports = app;
